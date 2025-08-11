c7658a35caf6d116e46f71d243029c3b32a4026d
Joel H. <143587522+Joelh123@users.noreply.github.com>
2025-08-11 12:20:00 +0300

    6.15

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,19 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit"
 
-const getId = () => (100000 * Math.random()).toFixed(0)
-
 const anecdoteSlice = createSlice({
   name: 'anecdote',
   initialState: [],
   reducers: {
     addAnecdote(state, action) {
-      const newAnecdote = {
-        content: action.payload,
-        id: getId(),
-        votes: 0
-      }
-    
-      state.push(newAnecdote)    
+      state.push(action.payload)   
     },
     voteAnecdote(state, action) {
       const id = action.payload
