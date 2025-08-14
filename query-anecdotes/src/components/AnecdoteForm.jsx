ea9576e021ef00ba783c65db394891f22ae1ff4f
Joel H. <143587522+Joelh123@users.noreply.github.com>
2025-08-14 09:12:42 +0300

    6.21

--- query-anecdotes/src/components/AnecdoteForm.jsx
+++ query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,21 +1,32 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createAnecdote } from "../services/requests";
+
 const AnecdoteForm = () => {
+	const queryClient = useQueryClient();
 
-  const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+	const newAnecdoteMutation = useMutation({
+		mutationFn: createAnecdote,
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ["anecdotes"] });
+		},
+	});
 
-  return (
-    <div>
-      <h3>create new</h3>
-      <form onSubmit={onCreate}>
-        <input name='anecdote' />
-        <button type="submit">create</button>
-      </form>
-    </div>
-  )
-}
+	const onCreate = (event) => {
+		event.preventDefault();
+		const content = event.target.anecdote.value;
+		event.target.anecdote.value = "";
+		newAnecdoteMutation.mutate({ content, votes: 0 });
+	};
 
-export default AnecdoteForm
+	return (
+		<div>
+			<h3>create new</h3>
+			<form onSubmit={onCreate}>
+				<input name="anecdote" />
+				<button type="submit">create</button>
+			</form>
+		</div>
+	);
+};
+
+export default AnecdoteForm;
--- query-anecdotes/src/components/Notification.jsx
+++ query-anecdotes/src/components/Notification.jsx
@@ -1,18 +1,15 @@
 const Notification = () => {
-  const style = {
-    border: 'solid',
-    padding: 10,
-    borderWidth: 1,
-    marginBottom: 5
-  }
-  
-  if (true) return null
+	const style = {
+		border: "solid",
+		padding: 10,
+		borderWidth: 1,
+		marginBottom: 5,
+	};
 
-  return (
-    <div style={style}>
-      
-    </div>
-  )
-}
+	// eslint-disable-next-line no-constant-condition
+	if (true) return null;
 
-export default Notification
+	return <div style={style}></div>;
+};
+
+export default Notification;
